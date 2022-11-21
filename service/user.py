001e8a56ddda5f65c972ff4a12e5875d4eb355a0
andrey.isaev <an.isaev7@gmail.com>
2022-11-21 22:46:33 +0300

    step 1

---
+++ service/user.py
@@ -0,0 +1,29 @@
+import hashlib
+
+from constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
+
+
+class UserService:
+    def __init__(self, dao):
+        self.dao = dao
+
+    def get_all(self):
+        return self.dao.get_all()
+
+    def get_one(self, uid):
+        return self.dao.get_one(uid)
+
+    def create(self, user_data):
+        return self.dao.create(user_data)
+
+    def update(self, user_data):
+        self.dao.update(user_data)
+        return self.dao
+
+    def generate_password(self, password):
+        return hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),  # Convert the password to bytes
+            PWD_HASH_SALT,
+            PWD_HASH_ITERATIONS
+        ).decode("utf-8", "ignore")
---
+++ views/users.py
@@ -0,0 +1,27 @@
+from flask import request
+from flask_restx import Resource
+
+from dao.model.user import UserSchema
+from implemented import user_service
+
+
+class UsersViews(Resource):
+    def get(self):
+        users = user_service.get_all()
+        return UserSchema(many=True).dump(users), 200
+
+    def post(self):
+        req_json = request.json
+        user = user_service.create(req_json)
+        return "", 201
+
+
+class UserViews(Resource):
+    def get(self, uid):
+        user = user_service.get_one(uid)
+        return UserSchema().dump(user), 200
+
+    def put(self):
+        req_json = request.json
+        user_service.update(req_json)
+        return "", 204
